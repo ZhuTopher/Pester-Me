1fffc74c84afdd6d1919cd6bac33e61ef2a45b82
Chris Zhu <christopher.zhu.9@gmail.com>
2016-08-01 10:59:51 -0400

    Refactor project folders

--- android/app/src/main/java/chris/nagme/Activities/MainActivity.java
+++ android/app/src/main/java/chris/nagme/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package chris.nagme;
+package chris.nagme.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import chris.nagme.R;
 
 public class MainActivity extends AppCompatActivity {
 
