bc53ee0679a9952a8a2bdc46a56893dfa366cb82
Chris Zhu <christopher.zhu.9@gmail.com>
2016-08-12 08:32:15 -0400

    Fill out Task base object as an abstract class

--- android/app/src/main/java/chris/PesterMe/Models/Task.java
+++ android/app/src/main/java/chris/PesterMe/Models/Task.java
@@ -4,6 +4,28 @@ package chris.PesterMe.Models;
  * Created by chris on 8/11/16.
  */
 
-public class Task { // TODO: use as an abstract class: ReminderTask, SubTask, MainTask (?)
+public abstract class Task { // TODO: use as an abstract class: SubTask, MainTask (?)
 	// TODO: Task object, needs various fields including a list of subtasks
+	private String title, description;
+
+	protected Task(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+
+	protected void setTitle(String title) {
+		this.title = title;
+	}
+
+	protected void setDescription(String description) {
+		this.description = description;
+	}
+
+	protected String getTitle() {
+		return this.title;
+	}
+
+	protected String getDescription() {
+		return this.description;
+	}
 }
